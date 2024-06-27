1f80e1df188536c552e7a34c8ea5fbeb3bba815d
Mohdlateef <lateefahbaba123@gmail.com>
2024-06-26 20:33:09 -0700

    first

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 
-let url='https://api.restful-api.dev/objects'
-fetch(url).then((res)=>{
-    console.log(res);
-    return res
-}).then((res.data)=>{
-    console.log(res.data)
-})
+let url="https://gauravgitacc.github.io/postAppData/posts.json"
+let container=document.getElementById("container");
+let button =document.getElementById("btn");
+button.addEventListener("click",getdata);
+async function getdata(){
+ let res=await fetch(url);
+ console.log(res)
+ let data=await res.json();
+ console.log(data)
+ data.map((item)=>{
+    let ele=document.createElement("div");
+    let del=document.createElement("button")
+    del.addEventListener("click",remove)
+    del.innerText="delete"
+    
+    console.log(item.title)
+    ele.innerHTML=`${item.title}`
+    ele.appendChild(del)
+
+    container.appendChild(ele);
+ })
+
+}
+function remove(e){
+    console.log(e.target.parentNode)
+    e.target.parentNode.remove();
+}
